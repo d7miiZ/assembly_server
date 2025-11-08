7daad80618e94051f120db44c83c77f7f43826a3
AbdulrahmanSaleh <55836752+d7miiZ@users.noreply.github.com>
2025-11-08 23:38:11 +0300

    chore: make it little bit smart

--- server.s
+++ server.s
@@ -3,6 +3,22 @@
 
 .section .text
 
+get_path:
+    mov rax, 0
+    add rdi, 4 # Skip "GET "
+   
+    path_loop:
+     mov cl, byte ptr [rdi + rax]
+     cmp cl, 0x20 # Is space
+     je path_exit
+     mov byte ptr [rsi + rax], cl
+     inc rax
+     jmp path_loop
+     
+    path_exit:
+     mov byte ptr [rsi + rax], 0
+     ret
+
 _start:
     mov rdi, 2
     mov rsi, 1
@@ -30,14 +46,42 @@ _start:
     mov r10, rax
 
     mov rdi, r10
-    lea rsi, [rip + read_buffer]
+    lea rsi, [rip + req_buffer]
     mov rdx, 1024
     mov rax, 0 # read
     syscall    
 
+    lea rdi, [rip + req_buffer]
+    lea rsi, [rip + path_buffer]
+    call get_path
+
+    lea rdi, [rip + path_buffer]
+    mov rsi, 0
+    mov rdx, 0
+    mov rax, 2 # open
+    syscall
+    mov rbx, rax
+
+    mov rdi, rbx
+    lea rsi, [rip + res_buffer]
+    mov rdx, 1024
+    mov rax, 0 # read
+    syscall
+    mov r12, rax
+
+    mov rdi, rbx
+    mov rax, 3 # close
+    syscall
+
     mov rdi, r10
     lea rsi, [rip + static_response]
     mov rdx, 19
+    mov rax, 1 # write
+    syscall   
+ 
+    mov rdi, r10
+    lea rsi, [rip + res_buffer]
+    mov rdx, r12
     mov rax, 1 # write
     syscall
 
@@ -51,7 +95,11 @@ _start:
 
 .section .bss
     .align 8
-read_buffer:
+req_buffer:
+    .skip 1024
+res_buffer:
+    .skip 1024
+path_buffer:
     .skip 1024
 
 .section .data
@@ -59,6 +107,6 @@ sockaddr_in:
     .word 2             # sin_family = AF_INET
     .word 0x5000        # sin_port = htons(80) → 0x0050 → stored as 0x5000  
     .long 0             # sin_addr = 127.0.0.1
-    .quad 0             # sin_zero[8] = 0i
+    .quad 0             # sin_zero[8] = 0
 static_response:
     .ascii "HTTP/1.0 200 OK\r\n\r\n"
